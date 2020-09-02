d5fb247ac46f67ecc963d08365f8d47c60a1d32a
Phuong Nguyen <maiphuongnguyen4101999@gmail.com>
2020-09-02 23:40:07 +0700

    Update
    demo
    
    Co-Authored-By: MaiPhuong410 <63777454+maiphuong410@users.noreply.github.com>

---
+++ QuanLyCuaHang_Web/QuanLyCuaHang/QuanLyCuaHang/Controllers/DemoController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using QuanLyCuaHang.Models;
+namespace QuanLyCuaHang.Controllers
+{
+    public class DemoController : Controller
+    {
+        // GET: Demo
+
+        QuanLyWebCoffeeDataContext db = new QuanLyWebCoffeeDataContext();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ToanBoSP(string searchString)
+        {
+            //List<SanPham> dsProduct;
+            //dsProduct = db.SanPhams.ToList();
+            var dsProduct = from sp in db.SanPhams select sp; //câu truy vấn
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                dsProduct = dsProduct.Where(s => s.TenSP.Contains(searchString));
+            }
+            return View("ToanBoSP", dsProduct);
+
+        }
+    }
+}
